146432f162764a60fa593fbbd930e2dd9cf3a9ca
Vincent Wang <51209452+vincentxwang@users.noreply.github.com>
2023-10-26 23:00:52 -0500

    perft function

--- src/tests/moves_test.rs
+++ src/tests/moves_test.rs
@@ -27,4 +27,37 @@ mod tests {
     }
      */
 
+    fn step_depth(boards: Vec<Board>) -> Vec<Board>{
+        let mut new_boards = Vec::new();
+        for board in boards {
+            let moves = Move::generate_legal_moves(&board);
+            for turn in moves {
+                let mut new_board = board.clone();
+                new_board.process_move(&turn);
+                new_boards.push(new_board);              
+            }
+        }
+        new_boards
+    }
+
+    #[test]
+    fn test_perft() {
+        let boards_0 = vec![Board::from_fen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1").unwrap()];
+        let boards_1 = step_depth(boards_0);
+        for board in boards_1 {
+            board.print_board();
+        }
+        // assert_eq!(boards_1.len(), 48);
+        /*
+        let boards_2 = step_depth(boards_1);
+        println!("{:?}", boards_2);
+        assert_eq!(boards_2.len(), 191);
+        let boards_3 = step_depth(boards_2);
+        assert_eq!(boards_3.len(), 2812);
+        let boards_4 = step_depth(boards_3);
+        assert_eq!(boards_4.len(), 43238);
+         */
+
+    }
+
 }
